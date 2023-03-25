42dd145f62b428eaf5c8a1b99a1bcf330432bc5b
Alagontor <86153710+Alagontor@users.noreply.github.com>
2023-03-25 18:37:56 +0400

    1

---
+++ js_homeworks/script.js
@@ -0,0 +1,20 @@
+// Типы данных приведенные к трем типам: Number, String, Boolean.
+
+let age = 25; // Number
+let eat = 'potato'; // String
+let fishswimming = true; // Boolean
+let bigint = 4356432175321569n; // Bigint
+let color = null; // Null
+let name; // Undefined
+let user = Object(); // Object
+let sym = Symbol(); // Symbol
+
+
+console.log(Number(age), String(age), Boolean(age));
+console.log(Number(eat), String(eat), Boolean(eat));
+console.log(Number(fishswimming), String(fishswimming), Boolean(fishswimming));
+console.log(Number(bigint), String(bigint), Boolean(bigint));
+console.log(Number(color), String(color), Boolean(color));
+console.log(Number(name), String(name), Boolean(name));
+console.log(Number(user), String(user), Boolean(user));
+console.log(Number(sym), String(sym), Boolean(sym));
