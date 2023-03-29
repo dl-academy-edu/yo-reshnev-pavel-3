756077dacd2b8941f14ef484adadfc424b65473f
Alagontor <86153710+Alagontor@users.noreply.github.com>
2023-03-29 15:45:14 +0400

    Домашка

--- js_homeworks/js_hom1/script.js
+++ js_homeworks/js_hom1/script.js
@@ -2,19 +2,21 @@
 
 let age = 25; // Number
 let eat = 'potato'; // String
-let fishswimming = true; // Boolean
+let isFishSwimming = true; // Boolean
 let bigint = 4356432175321569n; // Bigint
 let color = null; // Null
 let name; // Undefined
-let user = Object(); // Object
+let user = new Object(); // Object
 let sym = Symbol(); // Symbol
 
 
 console.log(Number(age), String(age), Boolean(age));
 console.log(Number(eat), String(eat), Boolean(eat));
-console.log(Number(fishswimming), String(fishswimming), Boolean(fishswimming));
+console.log(Number(isFishSwimming), String(isFishSwimming), Boolean(isFishSwimming));
 console.log(Number(bigint), String(bigint), Boolean(bigint));
 console.log(Number(color), String(color), Boolean(color));
 console.log(Number(name), String(name), Boolean(name));
 console.log(Number(user), String(user), Boolean(user));
 console.log(Number(sym), String(sym), Boolean(sym));
+
+
